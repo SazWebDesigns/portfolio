8772c29d281b5396885ca860c1c9afe6a5743086
Saz <sazwebdesigns@gmail.com>
2017-04-03 16:21:15 +0100

    adding icon images

---
+++ js/photoModal.js
@@ -0,0 +1,22 @@
+// var $modalBody= $(".modal-body");
+// var $image = $("<img>");
+// var $title = $("<h4></h4>");
+// var $buttons = $('<')
+
+// $modalBody.append($image);
+
+// $(".card a").click(function(event){
+// 	event.preventDefault();
+// 	var imageLocation = $(this).attr("href");
+
+// 	$image.attr("src", imageLocation);
+// 	$modalBody.show();
+
+// 	var titleText = $(this).children("img").attr("alt");
+// 	$title.text(titleText);
+// });
+
+// $overlay.click(function(){
+// 	$modalBody.hide();
+// });
+
